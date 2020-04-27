682bb9e6eeaccc4d7e5a06f5ec1b8fa7474595b1
Kurt Risley <kurtdrisley@gmail.com>
2020-04-27 15:48:26 -0600

    Day 1 commit

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : Controller
+    {
+        // GET /status -> 200 OK
+
+        [HttpGet("/status")]
+        public ActionResult GetTheStatus()
+        {
+            var response = new GetStatusResponse
+            {
+                Message = "Everything is golden!",
+                CheckedBy = "Kurt Risley",
+                WhenLastChecked = DateTime.Now
+            };
+            return Ok(response);
+        }
+    }
+
+    public class GetStatusResponse
+    {
+        public string Message { get; set; }
+        public string CheckedBy { get; set; }
+        public DateTime WhenLastChecked { get; set; }
+    }
+}
